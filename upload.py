007b1d8d40ac235d5a82f4f9e890be29e4273dbb
Tyler Mills <tyler.a.mills@gmail.com>
2017-06-05 11:24:22 -0400

    Better Logging

--- upload.py
+++ upload.py
@@ -2,10 +2,12 @@ import csv
 import requests
 import json
 
-# add token between quotes
+# add your authentication token between quotes
 authentication = ''
 
 endpoint = 'https://api.crimsonhexagon.com/api/content/upload'
+
+counter = 0
 
 def parse():
 
@@ -15,96 +17,117 @@ def parse():
 
     try:
 
+    	# open CSV file
         with open('upload.csv', 'rb') as file:
 
+        	# convert CSV file to a dictionary (key = column name, value = row value)
             reader = csv.DictReader(file, delimiter=',')
 
             print('Parsing file...')
 
             rowNumber = 1
-            print reader
 
+            # for each row in CSV file do stuff...
             for row in reader:
+                
+           		# for each key in the CSV dictionary, strip whitespace and add to variable (also check for all caps [e.g. TITLE vs title])
+                if row['title']: # make sure title exists
+                    title = row.get('title').strip() or row.get('TITLE').strip() 
+                
+                if row['author']: # make sure author exists
+                    author = row.get('author').strip().decode('utf-8') or row.get('AUTHOR').strip().decode('utf-8') # strip whitespace but also decode to deal with odd characters
+                
+                contents = row.get('contents').strip().decode('utf-8') or row.get('CONTENTS').strip().decode('utf-8')
 
-                if row['title']:
-                    title = row.get('title').strip() or row.get('TITLE').strip()
-                date = row.get('date') or row.get('DATE')
-                if row['author']:
-                    author = row.get('author') or row.get('AUTHOR')
                 url = row.get('url') or row.get('URL')
-                contents = row.get('contents').strip() or row.get('CONTENTS').strip()
+                date = row.get('date') or row.get('DATE')
                 content_type = row.get('type') or row.get('TYPE')
                 language = row.get('language') or row.get('LANGUAGE')
+                
+                try:
 
-                payload = {
-                    'title': title,
-                    'date': date,
-                    'author': author,
-                    'url': url,
-                    'contents': contents,
-                    'language': language,
-                    'type': content_type
-                }
-
-                if 'country' and 'state' and 'city' in row:
-
-                    if not row.get('country') or not row.get('state') or not row.get('city'):
-                        print('Row {} missing geolocation.'.format(rowNumber))
-
-                    geolocation = {
-                        'id': '{}.{}.{}'.format(row.get('country'), row.get('state'), row.get('city'))
+                	# create a document from the CSV dictionary "row" variables we just created
+                    payload = {
+                        'title': title,
+                        'date': date,
+                        'author': author,
+                        'url': url,
+                        'contents': contents,
+                        'language': language,
+                        'type': content_type
                     }
 
-                    payload['geolocation'] = geolocation
+                    # check if document includes country, state, and city string, if exists add to document payload
+                    if 'country' and 'state' and 'city' in row:
 
-                elif 'latitude' and 'longitude' in row:
-
-                    if not row.get('latitude') or not row.get('longitude'):
-                        print('Row {} missing geolocation.'.format(rowNumber))
-
-                    if row['latitude'] and row['longitude']:
+                        if not row.get('country') or not row.get('state') or not row.get('city'):
+                            print('Row {} missing geolocation.'.format(rowNumber))
 
                         geolocation = {
-                            'latitude': float(row.get('latitude')),
-                            'longitude': float(row.get('longitude'))
+                            'id': '{}.{}.{}'.format(row.get('country'), row.get('state'), row.get('city'))
                         }
 
                         payload['geolocation'] = geolocation
 
-                elif 'zipcode' in row:
+                    # if no country,state,city then try for lat/long, if exists add to document payload
+                    elif 'latitude' and 'longitude' in row:
 
-                    if not row.get('zipcode'):
-                        print('Row {} missing geolocation.'.format(rowNumber))
+                        if not row.get('latitude') or not row.get('longitude'):
+                            print('Row {} missing geolocation.'.format(rowNumber))
 
-                    geolocation = {
-                        'zipcode': row.get('zipcode'),
-                    }
+                        if row['latitude'] and row['longitude']:
 
-                    payload['geolocation'] = geolocation
+                            geolocation = {
+                                'latitude': float(row.get('latitude')),
+                                'longitude': float(row.get('longitude'))
+                            }
 
-                if 'gender' in row:
-                    if row['gender']:
-                        gender = row.get('gender')
-                    payload['gender'] = gender
-                    if not row.get('gender'):
-                        print('Row {} missing gender.'.format(rowNumber))
+                            payload['geolocation'] = geolocation
 
-                if 'age' in row:
-                    if row['age']:
-                        age = row.get('age')
-                    payload['age'] = int(age)
-                    if not row.get('age'):
-                        print('Row {} missing age.'.format(rowNumber))
+                    # if no country,state,city OR lat/long try zipcode, if exists add to document payload
+                    elif 'zipcode' in row:
 
-                documents.append(payload)
-                print json.dumps(payload)
+                        if not row.get('zipcode'):
+                            print('Row {} missing geolocation.'.format(rowNumber))
 
-                # if 1000 documents ready to upload, yield the documents to upload, erase the list of documents and continue on
-                if len(documents) == 1000:
-                    yield documents
-                    documents[:] = []
+                        geolocation = {
+                            'zipcode': row.get('zipcode'),
+                        }
 
-                rowNumber = rowNumber + 1
+                        payload['geolocation'] = geolocation
+
+                    # get gender if it exists and add to document payload
+                    if 'gender' in row:
+                        if row['gender']:
+                            gender = row.get('gender')
+                        payload['gender'] = gender
+                        if not row.get('gender'):
+                            print('Row {} missing gender.'.format(rowNumber))
+
+                    # get age if it exists and add to document payload
+                    if 'age' in row:
+                        if row['age']:
+                            age = row.get('age')
+                        payload['age'] = int(age)
+                        if not row.get('age'):
+                            print('Row {} missing age.'.format(rowNumber))
+
+
+                    # add the document to the list of documents
+                    documents.append(payload)
+
+                    # if 1000 documents ready to upload, yield the documents to upload, erase the list of documents and continue on
+                    if len(documents) == 1000:
+                        yield documents
+                        documents[:] = []
+
+                    rowNumber = rowNumber + 1
+
+                except Exception as e:
+                    global counter
+                    counter += 1
+                    pass
+
 
     except IOError:
 
@@ -114,7 +137,9 @@ def parse():
 
 def upload():
 
-    for documents in parse():
+	# this is a generator, so it takes the yielded documents around line 120 and does things with them
+
+    for documents in parse(): 
 
         query_parameters = {
             'auth': authentication
